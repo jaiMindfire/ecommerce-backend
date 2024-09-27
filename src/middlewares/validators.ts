28a37db5cd8a2524d139cda89e6a645809d76a2c
jaiMindfire <jaib@mindfiresolutions.com>
2024-09-27 14:16:23 +0530

    checkout transaction

--- src/middlewares/validators.ts
+++ src/middlewares/validators.ts
@@ -1,7 +1,6 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-  body("username").notEmpty().withMessage("Username is required"),
   body("email")
     .isEmail()
     .withMessage("Valid email is required")
